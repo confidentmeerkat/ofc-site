a66bf6d9c64db6295031d31655937283e35d098e
Hiro Takahashi <confidentmeerkat@gmail.com>
2022-08-12 20:13:04 -0700

    next config modifie

--- next.config.js
+++ next.config.js
@@ -3,11 +3,6 @@ const withImages = require("next-optimized-images");
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    loader: "imgix",
-    path: "/",
-  },
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
